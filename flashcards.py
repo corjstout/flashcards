8a238f61a85dd5dcf97d47b2e1681c0e5d2f2ac5
Stout <corjstout@gmail.com>
2024-12-31 23:39:50 -0500

    initial refactor complete

--- flashcards.py
+++ flashcards.py
@@ -2,58 +2,80 @@ import pygame
 import sys
 import json
 import random
+from pathlib import Path
+from typing import Final
 
-pygame.init()
+BG_COLOR: Final = "#0a092d"
+FLASHCARD_COLOR: Final = "#2e3856"
+FLIPPED_COLOR: Final = "#595e6d"
+TEXT_COLOR: Final = "white"
+APP_CAPTION: Final = "Cantonese Flash Cards"
+FONT_PACKAGE: Final = "STHeiti Medium"
 
-SCREEN = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("Quiz App")
+class CardGame:
+    def __init__(self, card_set: Path):
+        self.card_turned = False
+        self.index = 0
+        self.quit_requested = False
+        pygame.init()
+        pygame.display.set_caption(APP_CAPTION)
+        self.font = pygame.font.SysFont(FONT_PACKAGE, 30)
+        self.screen = pygame.display.set_mode((800, 800))
+        self.screen.fill(BG_COLOR)
 
-BG_COLOUR = "#0a092d"
-FLASHCARD_COLOUR = "#2e3856"
-FLIPPED_COLOUR = "#595e6d"
-FONT = pygame.font.SysFont("STHeiti Medium", 30)
-SCREEN.fill(BG_COLOUR)
+        with open(card_set) as file:
+            unshuffled_demo_quiz_data = json.load(file)
+        keys = list(unshuffled_demo_quiz_data.keys())
+        random.shuffle(keys)
+        self.demo_quiz_data = {key: unshuffled_demo_quiz_data[key] for key in keys}
 
-with open("card_set_general.json") as file:
-    demo_quiz_data = json.load(file)
-keys = list(demo_quiz_data.keys())
-random.shuffle(keys)
-demo_quiz_data = {key: demo_quiz_data[key] for key in keys}
+    def blit_text(self, text: str, height: int):
+        text_object = self.font.render(text, True, TEXT_COLOR)
+        text_rect = text_object.get_rect(center=(400, height))
+        self.screen.blit(text_object, text_rect)
 
-card_turned = False
-index = 0
+    def get_user_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if pygame.key.get_pressed()[pygame.K_SPACE]:
+                    self.card_turned = not self.card_turned
+                elif pygame.key.get_pressed()[pygame.K_RIGHT] and self.index < len(self.demo_quiz_data) - 1:
+                    self.index += 1
+                    self.card_turned = False
+                elif pygame.key.get_pressed()[pygame.K_LEFT] and self.index > 0:
+                    self.index -= 1
+                    self.card_turned = False
+                elif pygame.key.get_pressed()[pygame.K_ESCAPE]:
+                    self.quit_requested = True
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if pygame.key.get_pressed()[pygame.K_SPACE]:
-                card_turned = not card_turned
-            elif pygame.key.get_pressed()[pygame.K_RIGHT] and index < len(demo_quiz_data) - 1:
-                index += 1
-                card_turned = False
-            elif pygame.key.get_pressed()[pygame.K_LEFT] and index > 0:
-                index -= 1
-                card_turned = False
-    
-    SCREEN.fill(BG_COLOUR)
-    pygame.draw.rect(SCREEN, FLASHCARD_COLOUR, (150, 250, 500, 300))
+    def play(self):
+        while not self.quit_requested:
+            # Get any user keypresses
+            self.get_user_input()
 
-    if not card_turned:
-        current_question = list(demo_quiz_data)[index]
-        current_question_object = FONT.render(current_question, True, "white")
-        current_question_rect = current_question_object.get_rect(center=(400, 400))
-        SCREEN.blit(current_question_object, current_question_rect)
-    else:
-        current_answer = list(demo_quiz_data.values())[index]
-        current_answer_object = FONT.render(current_answer, True, "white")
-        current_answer_rect = current_answer_object.get_rect(center=(400, 400))
-        SCREEN.blit(current_answer_object, current_answer_rect)
-    
-    current_index_object = FONT.render(f"{index+1}/{len(demo_quiz_data)}", True, "white")
-    current_index_rect = current_index_object.get_rect(center=(400, 600))
-    SCREEN.blit(current_index_object, current_index_rect)
-    
-    pygame.display.update()
+            # Generate text strings
+            progress_text = f"{self.index+1}/{len(self.demo_quiz_data)}"
+            if not self.card_turned:
+                card_text = list(self.demo_quiz_data)[self.index]
+            else:
+                card_text = list(self.demo_quiz_data.values())[self.index]
+            
+            # Render the card and its text strings
+            self.screen.fill(BG_COLOR)
+            pygame.draw.rect(self.screen, FLASHCARD_COLOR, (150, 250, 500, 300))
+            self.blit_text(text=card_text, height=400)
+            self.blit_text(text=progress_text, height=600)
+            pygame.display.update()
+
+        # Quit the game if requested
+        pygame.quit()
+
+def main():
+    game = CardGame(Path("card_set_general.json"))
+    game.play()
+
+if __name__ == "__main__":
+    main()
